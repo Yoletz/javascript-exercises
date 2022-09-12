a70850772417ea3752c8c60b05fb3ee4043b83c4
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-12 18:59:30 +0800

    Edit javascript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+  let getLarger = (n, m) =>
+    n > m ? n : m;
+  
+  let getAge = (person) => 
+    !person.yearOfDeath ? (new Date()).getFullYear() - person.yearOfBirth :
+    person.yearOfDeath - person.yearOfBirth;
+  
+  let ageArr = obj.map(getAge);
+  
+  let getLargest = (index) =>
+    index === ageArr.length - 2 ? getLarger(ageArr[index], ageArr[index + 1]) :
+    getLarger(ageArr[index], getLargest(index + 1));
 
+  let oldestAge = getLargest(0);
+
+  let getOldest = (person) =>
+    getAge(person) === oldestAge;
+
+  let oldestPersons = obj.filter(getOldest);
+
+  return oldestPersons[0];
 };
 
 // Do not edit below this line

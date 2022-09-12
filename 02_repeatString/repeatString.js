b9e9fdbf8d606a3c83bd92edf7367f20aa1f280b
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-12 13:08:40 +0800

    Edit javascript exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+  let addString = (i) =>
+    i == 0 ? "" :
+    i < 0 ? "ERROR" :
+    str + addString(i - 1);
 
+  return addString(count);
 };
 
 // Do not edit below this line

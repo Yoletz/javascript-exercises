ef965256661dbdbce4c6b8f59075a71d26a72395
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-12 13:23:38 +0800

    Edit javascript exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let reverse = (index) =>
+    index < 0 ? "" :
+    index == 0 ? str[index] :
+    str[index] + reverse(index - 1);
 
+  return reverse(str.length - 1);
 };
 
 // Do not edit below this line

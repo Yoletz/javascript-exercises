2f80d6f8af8717a3174f77c6dad71f887c54742c
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-12 15:47:02 +0800

    Edit javascript exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(n, m) {
+	return n + m;
 };
 
-const subtract = function() {
-	
+const subtract = function(n, m) {
+	return n - m;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+  let sumIter = (i) => 
+    i === numArr.length ? 0 :
+    numArr[i] + sumIter(i + 1);
+
+  return sumIter(0);
 };
 
-const multiply = function() {
+const multiply = function(numArr) {
+  let productIter = (i) => 
+    i === numArr.length ? 1 :
+    numArr[i] * productIter(i + 1);
 
+  return productIter(0);
 };
 
-const power = function() {
-	
+const power = function(n, exp) {
+  let powIter = (i) =>
+    i === 0 ? 1 :
+    powIter(i - 1) * n;
+
+	return powIter(exp);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  return n == 0 ? 1 :
+    factorial(n - 1) * n;
 };
 
 // Do not edit below this line

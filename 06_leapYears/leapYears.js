0ac4e9e240c9e7f8dd75305fa9a2914e1bbe99f1
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-12 14:55:32 +0800

    Edit javascript exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(num) {
+  let isDivisible = (n, m) =>
+    n % m == 0;
 
+  return isDivisible(num, 4) &&
+    !isDivisible(num, 100) ||
+    isDivisible(num, 4) &&
+    isDivisible(num, 100) &&
+    isDivisible(num, 400);
 };
 
 // Do not edit below this line

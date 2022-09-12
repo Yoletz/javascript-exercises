f487904c75fca0bbc756a2e05fe8da25ec6558cd
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-12 17:05:10 +0800

    Edit javascript exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let getLettersOnly = (i) =>
+    i === str.length ? "" :
+    str.charCodeAt(i) >= 65 &&
+    str.charCodeAt(i) <= 90 ||
+    str.charCodeAt(i) >= 97 &&
+    str.charCodeAt(i) <= 122 ? str[i] + getLettersOnly(i + 1) :
+    getLettersOnly(i + 1);
 
+  let reverseStr = (str, index) =>
+    index === 0 ? str[0] :
+    str[index] + reverseStr(str, index - 1);
+
+  let reducedStr = getLettersOnly(0).toLowerCase();
+  
+  return reducedStr == reverseStr(reducedStr, reducedStr.length - 1);
 };
 
 // Do not edit below this line

3171184604bcf6e234704cdbf076edad1584d4c2
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-12 14:29:46 +0800

    Edit javascript exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  let isWrongInput = (num) =>
+    typeof num !== "number" ||
+    num < 0 || num === NaN ||
+    num === Infinity;
 
+  let iterSum = (i) =>
+    i === end ? end :
+    i + iterSum(i + 1);
+
+  return isWrongInput(start) || isWrongInput(end) ? "ERROR" :
+    start > end ? sumAll(end, start) :
+    iterSum(start);
 };
 
 // Do not edit below this line

fa184835990f0115dcbfd10fd2a23dd2c6bd9759
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-12 17:29:05 +0800

    Edit javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  let num = parseInt(input);
 
+  return num === 1 || num === 0 ? num :
+    num < 0 ? "OOPS" :
+    fibonacci(num - 2) + fibonacci(num - 1);
 };
 
 // Do not edit below this line

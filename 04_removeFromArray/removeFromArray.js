262449e0833200aa12e524016c1890363ee3a79b
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-12 14:02:10 +0800

    Edit javascript exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(arr, ...args) {
+  let newArr = [];
+
+  outerLoop:
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < args.length; j++) {
+      if (arr[i] === args[j]) {
+        continue outerLoop;
+      }
+    }
+    newArr.push(arr[i]);
+  }
+
+  return newArr;
 };
 
 // Do not edit below this line

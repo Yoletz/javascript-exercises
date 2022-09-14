35eef0322bdab8840e217bb760f7964a113d6eaa
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-14 16:30:56 +0800

    Refactor javascript exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -3,10 +3,7 @@ const leapYears = function(num) {
     n % m == 0;
 
   return isDivisible(num, 4) &&
-    !isDivisible(num, 100) ||
-    isDivisible(num, 4) &&
-    isDivisible(num, 100) &&
-    isDivisible(num, 400);
+    (!isDivisible(num, 100) || isDivisible(num, 400));
 };
 
 // Do not edit below this line
